41379b2c448c5c23faed41caae33c33b61befde2
mrandreikonan <mr.andrei.konan@gmail.com>
2025-06-09 12:22:07 -0600

    409D

--- 409/D.py
+++ 409/D.py
@@ -1,4 +1,22 @@
-for i in range(int(input())):
-    best = ''
+for test_case in range(int(input())):
+    N = int(input())
     S = input()
+
+    l = 0
+    r = 0
+
+    for i in range(N-1):
+        if S[i] > S[i+1]:
+            l = i
+            break
     
+    r = -1
+    for j in range(l+1, N):
+        if S[l] < S[j]:
+            r = j-1
+            break
+    
+    if r == -1:
+        r = N-1
+
+    print(S[0:l] + S[l+1:r+1] + S[l] + S[r+1:N])
