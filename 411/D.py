c7b2172a72ba18bb4e725ff24d53276c7a89d1c1
mrandreikonan <mr.andrei.konan@gmail.com>
2025-06-23 11:09:47 -0600

    411D After

--- 411/D.py
+++ 411/D.py
@@ -1,17 +1,26 @@
+from collections import deque
+
 N, Q = map(int, input().split())
 S = [0] * (N+1)
-ver = ['']
+ver = [(-1,'')]
 
 for _ in range(Q):
     q = input().split()
     if int(q[0]) == 1:
         S[int(q[1])] = S[0]
     elif int(q[0]) == 2:
-        v = ver[S[int(q[1])]]
-        v += q[2]
-        ver.append(v)
+        v_old = ver[S[int(q[1])]]
+        v_new = (S[int(q[1])], q[2])
+        ver.append(v_new)
         S[int(q[1])] = len(ver) - 1
     else:
         S[0] = S[int(q[1])]
 
-print(ver[S[0]])
+v = ver[S[0]]
+dq = deque()
+
+while (v[0] != -1):
+    dq.appendleft(v[1])
+    v = ver[v[0]]
+
+print("".join(dq))
